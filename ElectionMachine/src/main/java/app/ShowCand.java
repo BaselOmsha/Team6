473ca52d59669c06688f1fbf6ef7ad9bf50ba21a
nori21000 <nori.alija@student.hamk.fi>
2022-04-04 13:05:24 +0300

    ShowCand.java created

---
+++ ElectionMachine/src/main/java/app/ShowCand.java
@@ -0,0 +1,48 @@
+package app;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import app.model.Candidate;
+import app.dao.Dao;
+
+@WebServlet(
+        name = "ShowInfo",
+        urlPatterns = {"/showInfo"}
+        )
+
+public class ShowCand extends HttpServlet {
+	
+	@Override
+	    public void doGet(HttpServletRequest request, HttpServletResponse response) 
+	            throws IOException, ServletException {
+
+	        // if sessions does not exist, create new one
+	        HttpSession session = request.getSession();
+	        String value = request.getParameter("candidate_id");
+	        int candidate_id = Integer.parseInt(value);
+	        
+	        Dao dao = new Dao();
+	        Candidate cand = dao.showCandInfo(candidate_id);
+
+	        session.setAttribute("showCandInfo", cand);
+
+	        RequestDispatcher rd = request.getRequestDispatcher("jsp/showCand.jsp");
+	        rd.forward(request, response);
+
+	    }
+
+	    @Override
+	    public void doPost(HttpServletRequest request, HttpServletResponse response) 
+	            throws IOException, ServletException {
+	        doGet(request, response);
+	    }
+
+	}
