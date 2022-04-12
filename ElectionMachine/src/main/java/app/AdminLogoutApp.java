0f4d76c61ca9699d8ef924560228fb576f9c8c31
Basel <basil.omsha@student.hamk.fi>
2022-04-12 14:46:14 +0300

    doc added to .gitignore and some more element comments created

--- ElectionMachine/src/main/java/app/AdminLogoutApp.java
+++ ElectionMachine/src/main/java/app/AdminLogoutApp.java
@@ -10,9 +10,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+/**
+  * This Controller handles the admin logout
+ * 
+ * @author team6 (Nori, Jesse, Basil) 
+ * version 0.3
+ */
 @WebServlet(name = "AdminLogoutApp", urlPatterns = { "/adminlogout" })
 public class AdminLogoutApp extends HttpServlet {
 	
+	/**
+	 *Requests cookies and sets the max age to 0 then invalidates sessions
+	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
     	response.setContentType("text/html");
