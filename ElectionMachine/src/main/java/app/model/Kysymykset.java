c1b9ef895837c445d3a0210e691abc7fd683018f
Basel <basil.omsha@student.hamk.fi>
2022-04-26 14:15:20 +0300

    update

--- ElectionMachine/src/main/java/app/model/Kysymykset.java
+++ ElectionMachine/src/main/java/app/model/Kysymykset.java
@@ -1,6 +1,16 @@
 package app.model;
 
-public class Kysymykset {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Kysymykset implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int kysymys_id;
 	private String kysymys;
 	
